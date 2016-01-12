68ff49f87b6082bf60adceb6b4ec039fc8213fcb
Zhangguhua <iuyo5678@gmail.com>
2016-01-12 15:28:00 +0000

    add run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,4 @@
 # EMAIL       :                                                                
 # description :  我的vpn的运行文件                                                                  
 ################################################################################
-<<<<<<< HEAD
 nohup ./bin/qtunnel -listen=:4128 -backend=:4129 -clientmode=true -secret=secret -crypto=rc4 >> /dev/null  &
-=======
-nohup ./bin/qtunnel -listen=:4127 -backend=139.162.14.59:4128 -clientmode=true -secret=secret -crypto=rc4 >> /dev/null  &
->>>>>>> 2ad16275ef9ff2b8bc325ea71dd32790cfc3dce4
